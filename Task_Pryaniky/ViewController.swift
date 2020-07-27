e1303bca6826c04aed232e90a87d3d116403ef3b
Anya <melxxova@gmail.com>
2020-07-27 19:47:39 +0500

    Initial Commit

---
+++ Task_Pryaniky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task_Pryaniky
+//
+//  Created by Анна Мельхова on 27.07.2020.
+//  Copyright © 2020 Anna Melkhova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
